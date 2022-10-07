ed1365901b725e6280684b16c5318d585477765b
José Cláudio <jose.c.lima.sp@gmail.com>
2022-10-07 11:38:32 -0300

    Update lab004.py

--- Faculdade/lab004.py
+++ Faculdade/lab004.py
@@ -1,4 +1,7 @@
-import os
+import os  # limpa terminal
+from random import randint, randrange  # nmr aleatorio
+from time import sleep  # freeza o terminal
+os.system('cls') or None
 
 # 2
 num = int(input(f'Digite um valor:\t'))
@@ -17,7 +20,51 @@ else:
         num -= 1
 
 print(f'O fatorial de {nini} é {fact}')
+sleep(3)
 
 # 3
+os.system('cls') or None
+
+c = int(input(f'Digite o valor que irá pegar emprestrado:\t'))
+t = int(input(f'Digite o tempo que terá para pagar o empréstimo:\t'))
+
+while c < 0 or t < 0:
+    c = int(input(f'Digite o valor que irá pegar emprestrado:\t'))
+    t = int(input(f'Digite o tempo que terá para pagar o empréstimo:\t'))
+
+
+i = 0.069
+if t < 12:
+    i = 0.0714
+elif t < 24:
+    i = 0.0921
+else:
+    i = 0.1246
+
+print(f'O valor a ser pago dentro de {t} meses, será {c*(1+i)**t:.2f}')
 
 # 4
+os.system('cls') or None
+
+random = randint(1, 10)
+chute = -1
+tentativa = 1
+chute = int(input(
+    f'Tente acertar o número aleatório,é sua {tentativa}° tentativa:\t'))
+
+while chute < 1 or chute > 10:
+    chute = int(input(f'Tente novamente, é de 1 a 10:\t'))
+
+while (chute != random):
+    tentativa += 1
+    if tentativa > 3:
+        print(f'Ultrapassou de 3 tentativas, você P-E-R-D-E-U!!')
+        break
+
+    chute = int(
+        input(f'Tente acertar o número aleatório,é sua {tentativa}° tentativa:\t'))
+    while chute < 1 or chute > 10:
+        chute = int(input(f'Tente novamente, é de 1 a 10:\t'))
+
+else:
+    print(f'Acertou na {tentativa}, o valor era {random}!')
