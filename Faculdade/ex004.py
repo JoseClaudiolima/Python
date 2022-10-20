24665bd3980306cf71129ee71e4c8cd34ceb51ea
José Cláudio <jose.c.lima.sp@gmail.com>
2022-10-20 13:16:21 -0300

    dicionario python

---
+++ Faculdade/ex004.py
@@ -0,0 +1,8 @@
+from os import system
+system('cls') or None
+
+dic = {'chave': 'ABD1521', 'compras': 'Mouse Gamer'}
+dic2 = {'chave': 'password123'}
+array = [0, 1]
+dic2.update(dic)
+print(dic2)
--- Faculdade/lab005.py
+++ Faculdade/lab005.py
@@ -1,4 +1,5 @@
 from os import system
+from time import sleep
 system('cls') or None
 
 # 1
@@ -17,11 +18,45 @@ else:
     print(f'{nome} irá se aposentar com {idade+60}, caso não tenha trabalhado ainda, e comesse no próximo ano')
 '''
 
+
 # 2
+system('cls') or None
+
 print("Aparantemente, você não tem cadastro ainda, faça o seu agora!")
-login = input(f'Cadastre o seu login: ').upper()
-while login.isalpha() is False:
-    if login.isalnum() and login[0].isnumeric is False:
-        break
-    print(f'Apenas algarismos alfanúmeros, e não pode começar com números!')
+print(f'Atenção, o seu login não pode começar com números e ter espaços!')
+login = input(f'Cadastre o seu login: ')
+while login.isalnum() is False or login[0].isnumeric():
+    print(f'Apenas algarismos alfanúmeros, e não pode começar com números nem ter espaços!')
     login = input(f'Cadastre o seu login: ').upper()
+
+print(f'\nLogin cadastrado com sucesso!')
+
+
+print(f'\nAgora vamos cadastrar a sua senha!')
+print(f'Sua senha deverá ter no minimo 8 caracteres e um caractere especial.')
+senha = input(f'Digite a sua senha: ')
+
+while len(senha) < 8:
+    print(f'Sua senha deverá ter no minimo 8 caracteres e um caractere especial.')
+    senha = input(f'Digite a sua senha: ')
+a = 0
+while a == 0:
+    for i in range(len(senha)):
+        if senha[i] in '!@#$%¨&*()':
+            a = 1
+
+print(f'Senha cadastrada com sucesso!')
+sleep(3)
+system('cls') or None
+ent = input(f'Entre com o seu Login: ')
+if ent == login:
+    ent = input(f'Entre com a sua Senha: ')
+    if ent == senha:
+        print(f'Bem vindo ao APP!')
+    else:
+        print(f'Senha Incorreta!')
+else:
+    print(f'Usuário não cadastrado!')
+
+
+# 3
