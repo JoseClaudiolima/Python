ee6a082011e47141cf938c299abec72f3ed0fbdb
José Cláudio <jose.c.lima.sp@gmail.com>
2022-10-27 14:52:34 -0300

    Update copia-criptografiaRSA.py

--- Criptografia/copia-criptografiaRSA.py
+++ Criptografia/copia-criptografiaRSA.py
@@ -5,7 +5,7 @@ system('cls') or None
 
 #Essa função retorna o mdc entre dois valores, usando o algoritmo de Euclides
 
-#Porém antes de calcular o algoritmo de Euclides, é feito uma comparação entre os valores entrados para saber qual é o maior valor.
+#Porém antes de calcular o algoritmo de Euclides, é feito uma comparação entre os valores entrados para saber qual é o maior valor, coloquei a comparação caso no futuro queira colocar números aleatórios ae. mas acho que não vamos usar. 
 def mdc(a,b): 
     if a >b:
         maior = a
@@ -22,20 +22,24 @@ def mdc(a,b):
     return maior
     
 
-# A função gera um número aleatório que seja acima de 1 e menor que o conjunto n (que é o tamanho de conjunto das nossas chaves privadas), além disso o número precisa ser co-primo de n, por isso é feito um looping para que o numero tenha mdc entre ele e n igual a 1.
+
+# A função gera um número aleatório que seja acima de 1 e menor que o conjunto 'n' (que é o tamanho de conjunto das nossas chaves privadas), além disso o número precisa ser co-primo de 'n', por isso é feito um looping para que o numero tenha mdc entre ele e n igual a 1.
 
 # Anotação: Números co-primos são aqueles números, que entre ELES não há divisor comum além de 1 (mdc entre eles é 1).
 
 def chave_publica(n):
     while True:
         e = random.randrange(2,n)
-        if mdc(n,e) == 1:
+        if mdc(n,e) == 1 and e !=145:
             return e
-#se e for '145' a msg cifragem é igual a msg decifrada, e se for '127' mesmo a chave privada sendo igual a chave publica, isso não acontece. isso se n for 323
+#se e for '145' a msg cifragem é igual a msg decifrada, e se for '127' mesmo a chave privada sendo igual a chave publica, isso não acontece. isso se n for 323 (p=17 q=19)
 
 
 
-#Pesquisar depois sobre Inverso multiplicativo
+#Pesquisar depois sobre Inverso multiplicativo, inverso multiplicativo é o número mutiplicado para o resultado ser 1. ou seja, 10 . y = 1 --> y = 0,1......5 . y = 1 --> y = 0,5
+
+#Achei complexo e não entendi, ele usou algoritmo de euclides estendidio para calcular o inverso multiplicativo de 'e' em uma equação estranha.
+#link :lambda3.com.br/2013/01/entendendo-de-verdade-a-criptografia-rsa-parte-iii/
 def chave_privada(totiente, e):
     d = 0
     while ((d* e) % totiente != 1):
@@ -45,7 +49,7 @@ def chave_privada(totiente, e):
 
 #A cifragem em RSA, é feita letra a letra pela seguinte equação: lcifra = l ** chavepub % n
 
-#Para realizar a cifragem, temos que ter definido a mensagem, chave publica (que é a de ciptografia)  o conjunto n.
+#Para realizar a cifragem, temos que ter definido a mensagem, chave publica (que é a de ciptografia) e o conjunto n.
 # Tendo isso podemos aplicar a formula acima, porém a lógica por de trás dessa função é: transformamos letra a letra por vez em um número, e após isso combinamos (com exponenciação) com a chave publica, com isso o numero da letra cifrada aumentará muito podendo até passar do conjunto n estabelecido, para resolver esse problema, é necessário pegar o resto da divisão da letra cifrada com o conjunto, por fim temos a letra cifrada dentro do conjunto estabelecido.
 def cifrar(mensagem,e,n):
     msg_cifrada = ''
@@ -66,24 +70,25 @@ def decifrar(mensagem,n,d):
 
 
 def rsa ():
-    msg = 'Você é pica, José!'
+    msg = 'Deu bom, confia'
 
     #Escolheu a chave privada, quando escolheu esses numeros primos
-    p = 211 #211
-    q = 191 #191
+    p = 19 #211
+    q = 17 #191
     n = p * q #323   
 
-    #Este n é o tamanho do nosso conjunto. É necessário termos um conjunto finito de valores para que possamos fazer o caminho inverso ao realizado para cifrar nossa mensagem. Podemos, chamar nosso conjunto de 323.
+    #Este n é o tamanho do nosso conjunto. No site diz que "É necessário termos um conjunto finito de valores para que possamos fazer o caminho inverso ao realizado para cifrar nossa mensagem". Podemos, chamar nosso conjunto de 323.
 
 
     # Daqui para frente é a função totiente, totiente de n (323)
+    #youtube.com/watch?v=3MryVNzS3o4&ab_channel=KhanAcademyBrasil explica totiente de forma facil
     # Totiente significa: a quantidade de co-primos de um numero que são menores que ele mesmo.
-    # O totiente são os dois números primos escolhidos menos 1 cada, seguido de multiplicação 
-    #(x) = (p - 1) * (q - 1)
+    # O totiente de dois numeros primos são os dois números escolhidos menos 1 cada, seguido de multiplicação 
+    #Φ(x) = (p - 1) * (q - 1)
     
     totiente = (p - 1) * (q - 1)
 
-    #Ao chamar a função de chave publica com o parâmetro de totiente, estamos pedindo que gere um número primo aleatório.
+    #Ao chamar a função de chave publica com o parâmetro de totiente, estamos pedindo que gere um número co-primo aleatório.
     e = chave_publica(totiente)
 
     print(f'Chave publica: ({e},{n})')
@@ -98,4 +103,3 @@ def rsa ():
     print(f'Mensagem Decifrada: {msg}')
 
 rsa()
-print(f'Deu certo')
